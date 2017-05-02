8475e71db1a1206d81ab7180664d5668b301941e
15017523 <haziqnahar20@gmail.com>
2017-05-02 21:48:44 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a15017523/demoexplicitintent/Hero.java
@@ -0,0 +1,30 @@
+package com.example.a15017523.demoexplicitintent;
+
+import java.io.Serializable;
+
+/**
+ * Created by 15017523 on 2/5/2017.
+ */
+
+public class Hero implements Serializable{
+    private String name;
+    private int strength;
+    private int technicalProwess;
+
+    public String getName() {
+        return name;
+    }
+    public int getStrength() {
+        return strength;
+    }
+    public int getTechnicalProwess() {
+        return technicalProwess;
+    }
+    public Hero(String name, int strength,
+                int technicalProwess) {
+        this.name = name;
+        this.strength = strength;
+        this.technicalProwess = technicalProwess;
+    }
+
+}
